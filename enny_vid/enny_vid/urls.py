7c56fe5f0567a62370310b93ec9903a6c4092d52
teefats <tee_fats@yahoo.com>
2021-01-13 19:58:21 +0000

    Added module diting functionality

--- enny_vid/enny_vid/urls.py
+++ enny_vid/enny_vid/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls'))
   
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
